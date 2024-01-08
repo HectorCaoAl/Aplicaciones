0d75e6da8850c3882cbf0b16274395bf13bd4af6
HectorCaoAl <hectorcaoal@gmail.com>
2024-01-08 08:07:01 +0000

    se añade parametros por defecto

--- hola.php
+++ hola.php
@@ -1,2 +1,7 @@
 <?php
+// El nombre por defecto es Mundo
+$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+@print "Hola, {$nombre}\n";<?php
+$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+@print "Hola, {$nombre}\n";<?php
 @print "Hola {$argv[1]}\n";
