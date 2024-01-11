8b41fbbc977017754a3d482f68cd886c5874daec
HectorCaoAl <hectorcaoal@gmail.com>
2024-01-11 11:47:38 +0000

    s

--- hola.php
+++
@@ -1,7 +0,0 @@
-<?php
-//  El nombre por defecto es Mundo
-$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
-@print "Hola, {$nombre}\n";<?php
-$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
-@print "Hola, {$nombre}\n";<?php
-@print "Hola {$argv[1]}\n";
